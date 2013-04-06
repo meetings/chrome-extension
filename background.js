63382bc8953c7c913d35491a1a9d2f0364468135
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2013-04-06 14:44:53 +0300

    oninstall added

---
+++ background.js
@@ -0,0 +1,25 @@
+function onInstall() {
+  chrome.tabs.create({
+    url:  "http://chrome.meetin.gs/"
+  });
+}
+
+function onUpdate() {
+
+}
+
+function getVersion() {
+  var details = chrome.app.getDetails();
+  return details.version;
+}
+
+var currVersion = getVersion();
+var prevVersion = localStorage['version']
+if (currVersion != prevVersion) {
+  if (typeof prevVersion == 'undefined') {
+    onInstall();
+  } else {
+    onUpdate();
+  }
+  localStorage['version'] = currVersion;
+}
